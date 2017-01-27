5055cfb2470583f516fb42c99a02e07f1c26e248
Samuel Saunders <sam.saunders116@yahoo.com>
2017-01-27 06:34:17 -0500

    initial

---
+++ Key2success/CheckingAcc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Key2success
+{
+    class CheckingAcc
+    {
+        private int withDrawfunds;
+        private int depositFunds;
+
+
+        public int DepositFunds
+        {
+            get { return this.DepositFunds; }
+            set { this.DepositFunds = value; }
+        }
+
+        public int WithDrawfunds
+        {
+            get { return this.WithDrawfunds; }
+            set { this.WithDrawfunds = value; }
+        }
+    }
+}
